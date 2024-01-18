3569ec7e0ff137643f8daa647dfd74af5e4f190b
jonasarce1 <jonasarceg@gmail.com>
2024-01-18 10:02:37 +0100

    mongo

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 
-/*const MONGO_URL = Deno.env.get("MONGO_URL");
+const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
   console.log("No mongo URL found");
   Deno.exit(1);
 }
 
-await mongoose.connect(MONGO_URL);*/
+await mongoose.connect(MONGO_URL);
 
 const app = express(); 
 app.use(express.json());
